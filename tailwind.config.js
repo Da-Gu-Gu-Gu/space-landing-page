03f7eef8b0f3d335342a7fd8fb62c3766049d9fc
tankamout <mountchitmyrsuu@gmail.com>
2022-07-19 20:51:48 +0630

    intial state

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        db: "#0b0d17",
+        lb: "#d0d6f9",
+      },
+    },
+  },
+  plugins: [],
+};
